423d6d9d095a25d13c768655f2158526e1d4ad44
Ishan Dassanayake <ishansd94@gmail.com>
2020-12-01 14:48:09 +0530

    New backup 2020-12-01 14:48:09

---
+++ README.md
@@ -0,0 +1,10 @@
+[] Run the pre script
+[] Software Packages
+    [] Jetbrains Toolbox
+    [] SourceTree
+    [] Authy
+    [] Bitwarden
+    [] Enpass
+    [] Spotify
+    [] VS Code
+    [] Opera
---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp -r ~/bin .
+
+ls -l /usr/local/bin > ./packages/usr_local_bin.txt
+brew list -1 > ./packages/brew_list.txt
+brew list --casks -1 > ./packages/brew-cask_list.txt
+ls -l /usr/local/bin | egrep -iv "Cellar|Applications" > ./packages/system_apps_list.txt
+
+#Check git status
+git_status="$(git status | grep -i "add\|modified\|new")"
+
+if [[ $git_status != ""  ]]; then
+  echo "changes in setup files"
+  git add . ;
+  git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+  git push;
+fi  
+
+#TTest for auto backup
